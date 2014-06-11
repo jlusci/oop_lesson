37ecb2d0258f6386432df736f10dd30f1857ace7
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 12:52:00 -0700

    up through step 12

--- game.py
+++ game.py
@@ -11,13 +11,91 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+    SOLID = True
+
+class Character(GameElement):
+    IMAGE = "Princess"
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+
+class Gem(GameElement):
+    IMAGE = "OrangeGem"
+    SOLID = False
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2, 2, PLAYER)
+    print PLAYER
+
+    rock_positions = [
+            (2, 1),
+            (1, 2),
+            (3, 2),
+            (2, 3)
+        ]
+    #how to fill up entire board!
+    # rock_positions = []
+    # for i in range(GAME_WIDTH):
+    #     for j in range(GAME_HEIGHT):
+    #         rock_positions.append((i, j))
+
+    rocks = []
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
+
+    rocks[-1].SOLID = False
+
+    GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
+
+def keyboard_handler():
+    direction = None
+
+    if KEYBOARD[key.UP]:
+        direction = "up"
+    if KEYBOARD[key.DOWN]:
+        direction = "down"
+    if KEYBOARD[key.LEFT]:
+        direction = "left"
+    if KEYBOARD[key.RIGHT]:
+        direction = "right"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el is None or not existing_el.SOLID:
+            # If there's nothing there _or_ if the existing element is not solid, walk through
+            GAME_BOARD.del_el(PLAYER.x,PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
