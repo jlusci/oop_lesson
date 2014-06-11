adab6f6b2f671542731cbfeb75fe794eaaaf1c1e
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 15:12:50 -0700

    Working to step 13.

--- game.py
+++ game.py
@@ -22,6 +22,10 @@ class Rock(GameElement):
 class Character(GameElement):
     IMAGE = "Princess"
 
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
     def next_pos(self, direction):
         if direction == "up":
             return (self.x, self.y-1)
@@ -36,6 +40,11 @@ class Character(GameElement):
 class Gem(GameElement):
     IMAGE = "OrangeGem"
     SOLID = False
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" % (len(player.inventory)))
+        
 ####   End class definitions    ####
 
 def initialize():
@@ -75,6 +84,7 @@ def initialize():
     gem = Gem()
     GAME_BOARD.register(gem)
     GAME_BOARD.set_el(3, 1, gem)
+    GAME_BOARD.set_el(0, 1, gem)
 
 def keyboard_handler():
     direction = None
@@ -94,6 +104,9 @@ def keyboard_handler():
         next_y = next_location[1]
 
         existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el:
+            existing_el.interact(PLAYER)
 
         if existing_el is None or not existing_el.SOLID:
             # If there's nothing there _or_ if the existing element is not solid, walk through
